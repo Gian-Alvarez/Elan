82dbee311a77e9ba699a233bcc2a7c87a7cebda0
COP Group <cop43318@gmail.com>
2022-08-02 11:12:43 -0400

    Fourteenth Commit

--- frontend/src/components/AddWeight.js
+++ frontend/src/components/AddWeight.js
@@ -57,12 +57,12 @@ function AddWeight()
     return(
         <div id="AddWeightDiv">
             <form onSubmit={doAddWeight}>
-            <span id="inner-title">ADD WEIGHT</span><br />
+            <span id="inner-title">Log Your Weight</span><br />
             <input type="date" id="date" placeholder="Type In Date" 
             ref={(c) => date = c} /> <br />      
             <input type="number" id="weight" placeholder="Weight" 
             ref={(c) => weight = c} /> <br />        
-            <input type="submit" id="addWeightButton" class="buttons" value = "Do It"
+            <input type="submit" id="addWeightButton" class="buttons" value = "Log Weight"
             onClick={doAddWeight} />
             </form>
             <span id="addWeightResult">{message}</span>
